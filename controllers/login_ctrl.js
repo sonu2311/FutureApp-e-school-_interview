51807d11d5ee04bde089755d4f6063495a2004ef
Sonu Saini <sonu2311saini@gmail.com>
2022-08-21 14:34:13 +0530

    final fixup

--- controllers/login_ctrl.js
+++ controllers/login_ctrl.js
@@ -1,15 +1,14 @@
 app.controller('login_ctrl', function($scope, $http) {
-  s = $scope
-  $scope.username="mor_2314"
-  $scope.password="83r5^_"
+  $scope.username=""
+  $scope.password=""
   $scope.is_sign_up_disabled=false
   $scope.show_password_icon = false
+
   $scope.password_hide_or_show=function(){
       $scope.show_password_icon = !$scope.show_password_icon
   }
-  $scope.login=function(){
 
-    
+  $scope.login=function(){ 
     if (!(/^[a-z0-9_.]+$/).test($scope.username)){
       $scope.my_alert("username is not correct.")
       return
@@ -21,14 +20,12 @@ app.controller('login_ctrl', function($scope, $http) {
     })
     .then(function(backend_output){
       $scope.is_login_disabled=false
-      console.log("backend_output==", backend_output)
       localStorage.token = backend_output.data.token
       window.location.href = "#!/products_page"
     })
     .catch(function(e){
       $scope.my_alert(e.data)
       $scope.is_login_disabled=false
-      console.log("e===", e)
     })
   }
 });
--- main.js
+++ main.js
@@ -4,11 +4,11 @@ var app = angular.module('myApp', ["ngRoute"])
 app.config(function($routeProvider) {
     $routeProvider
     .when("/login", {
-        templateUrl : "includes/login.html"
+      templateUrl : "includes/login.html"
     })
     .when("/", {
-        templateUrl : "includes/dashboard.html",
-        subPage: "includes/products_page.html"
+      templateUrl : "includes/dashboard.html",
+      subPage: "includes/products_page.html"
     })
     .when("/products_page", {
       templateUrl : "includes/dashboard.html",
@@ -21,23 +21,22 @@ app.config(function($routeProvider) {
   ;
 });
 
-app.controller('products_ctrl', function($scope, $route, $http) {
-  $scope.self =$scope
+app.controller('products_ctrl', function($scope, $http) {
   $scope.products_list=[]
-  $scope.sort_by = "asc"
-  $scope.limit = "20"
+  $scope.inputs = {
+    sort_by: "asc",
+    limit: "20"}
 
   $scope.reload = function() {
-    $http.get('https://fakestoreapi.com/products?sort='+$scope.sort_by+'&limit='+$scope.limit)
+    $http.get('https://fakestoreapi.com/products?sort='+$scope.inputs.sort_by+'&limit='+$scope.inputs.limit)
     .then(function(backend_output){
-      console.log("backend_output_of products==", backend_output)
       $scope.products_list =backend_output.data
     })
   }  
   $scope.reload()
 });
 
-app.controller('single_product_ctrl', function($scope, $route, $http) {
+app.controller('single_product_ctrl', function($scope,  $http) {
   $scope.product_info={}
 
   var url_params = new URLSearchParams(window.location.href.split("?")[1])
@@ -74,58 +73,6 @@ app.controller('dashboard_ctrl', function($scope, $route) {
   $scope.subPage = $route.current.$$route.subPage;
 });
 
-app.controller('PropertyCtrl', function($scope,$http) {
-   $scope.detail_show=false;
-   console.log($scope.detail_show)
-   $scope.show_hide_details=function(){
-    $scope.detail_show=!$scope.detail_show
-    console.log($scope.detail_show)
-   }
 
-   $scope.selected_tab='mortage'
-   $scope.select_tab=function(x){
-    $scope.selected_tab=x
-   }
-
-   $scope.realpha_dropdown='realpha_inputs'
-
-   var url_params = new URLSearchParams(window.location.href.split("?")[1])
-  $scope.url_property_id = url_params.get("id")
-  console.log("$scope.url_property_id = " + $scope.url_property_id)
-
-  $scope.favorite_icon= false
-
-  if($scope.url_property_id !=null) {
-    api($http, '/get_property_info', {"property_id":$scope.url_property_id}, function(backend_output){
-      $scope.property_info = backend_output.info
-      console.log($scope.property_info)
-      $scope.favorite_icon= backend_output.favorite_icon_backend_output
-      console.log($scope.favorite_icon)
-      $scope.my_alert("loaded.")
-    })
-  }
-
-  $scope.mark_favorite=function(){
-    api($http, '/mark_favorite', {"property_id":$scope.url_property_id}, function(backend_output){
-        $scope.favorite_icon= !$scope.favorite_icon
-      })
-  }
-
-  $scope.image_list=["https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg",
-  "https://cdn.vox-cdn.com/thumbor/frFQQhOsxl8DctGjkR8OLHpdKMs=/0x0:3686x2073/1200x800/filters:focal(1549x743:2137x1331)/cdn.vox-cdn.com/uploads/chorus_image/image/68976842/House_Tour_Liverman_3D6A3138_tour.0.jpg", 
-  "https://i.pinimg.com/originals/4d/9d/d6/4d9dd658da8a62f5f9726c903af2be98.jpg", 
-  "https://i.pinimg.com/originals/78/83/46/7883462c95aca4252546e9963187f4e6.jpg"
-  ]
-  $scope.change_image=0
-  $scope.property_next_image=function(){
-    $scope.change_image=($scope.change_image+1)% $scope.image_list.length
-  }
-
-  $scope.property_previous_image=function(){
-    $scope.change_image=($scope.change_image - 1 + $scope.image_list.length) % $scope.image_list.length
-
-  }  
-
-})
   
 
